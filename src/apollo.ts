df202c24f0396b4b6172ee14a4d8c633d0cbf142
Vinicius Gularte <viniciusgulartemartin@gmail.com>
2019-11-25 21:52:37 -0300

    adicionando simples listagem

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,7 +1,14 @@
 import ApolloClient from "apollo-boost";
+import { LAUNCHES_PAST_QUERY } from "./graphql/launches";
 
 const uri = process.env.REACT_APP_API_URL;
 const client = new ApolloClient({
   uri
 });
+client
+  .query({
+    query: LAUNCHES_PAST_QUERY
+  })
+  .then(result => console.log("Launches", result))
+  .catch(err => console.log("Erro: ", err));
 export { client };
