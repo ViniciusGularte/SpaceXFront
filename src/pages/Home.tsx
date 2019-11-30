14ecba89f4ab307c1aa313f2c1b41469735f8202
Vinicius Gularte <viniciusgulartemartin@gmail.com>
2019-11-30 19:21:40 -0300

    git fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,11 +7,9 @@ import {
   IonItem
 } from "@ionic/react";
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { LAUNCHES_PAST_QUERY } from "../graphql/launches";
-import { LaunchesPastQuery } from "../generated/graphql";
+import { useLaunchesPastQuery } from "../generated/graphql";
 const Home: React.FC = () => {
-  const { data, loading } = useQuery<LaunchesPastQuery>(LAUNCHES_PAST_QUERY);
+  const { data, loading } = useLaunchesPastQuery();
   return (
     <IonPage>
       <IonHeader>
@@ -25,8 +23,8 @@ const Home: React.FC = () => {
         ) : (
           data &&
           data.launchesPast!.map(launch => (
-            <IonItem key={launch!.id as string}>
-              {launch!.mission_name} | {launch!.rocket!.rocket_name}
+            <IonItem key={launch.id as string}>
+              {launch.mission_name} | {launch.rocket.rocket_name}
             </IonItem>
           ))
         )}
